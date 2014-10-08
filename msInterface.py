4f6b90ffb7536bc220f7a45a7516c2d4d1f6dd92
☞ <kthoden@mpiwg-berlin.mpg.de>
2014-10-08 18:10:59 +0200

    Also got redirect to work now, but still no success in really displaying things.

--- msInterface.py
+++ msInterface.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8; mode: python -*-
-# Time-stamp: <2014-10-08 14:37:38 (kthoden)>
+# Time-stamp: <2014-10-08 18:10:26 (kthoden)>
 
 __author__="Klaus Thoden"
 __date__="2014-03-13"
 __doc__ = """Getting a page out of the Confluence system and convert it to
 something the Eclipse Marketplace understands.
 https://dev2.dariah.eu/wiki/rest/prototype/1/content/27329537
-
-Do we need content negotiation?
-If the Lab calls, redirect it to /api/p and deliver the XML
-A browser might want to go to $MS/content/3 and expects the plugin info there
 
 For the pages to be served by the service then, we need several inputs:
 - the data parsed out of the corresponding wiki pages
@@ -63,12 +59,11 @@ Reference: http://wiki.eclipse.org/Marketplace/REST
         - [X] $baseURL/recent/api/p :: Recently updated or added listings: http://textgridlab.org/marketplace/recent/api/p
         - [X] $baseURL/favorites/top/api/p :: most favourite things: http://textgridlab.org/marketplace/favorites/top/api/p
         - [X] $baseURL/popular/top/api/p :: most popular by activity: http://textgridlab.org/marketplace/popular/top/api/p
-
-put info on installable Units also in config
-config should be kept in XML.
 """
 
-# imports
+###########
+# Imports #
+###########
 import argparse
 from lxml import etree
 from datetime import datetime
@@ -76,17 +71,18 @@ import cgi
 
 # had some big problems with getting the encoding right
 # answer on https://stackoverflow.com/questions/9322410/set-encoding-in-python-3-cgi-scripts
-# finally did the trick
-import locale                                  # Ensures that subsequent open()s 
-locale.getpreferredencoding = lambda: 'UTF-8'  # are UTF-8 encoded.
+# finally did the trick:
 
+# Ensures that subsequent open()s are UTF-8 encoded.
+import locale
+locale.getpreferredencoding = lambda: 'UTF-8'
 import sys                                     
-sys.stdin = open('/dev/stdin', 'r')       # Re-open standard files in UTF-8 
-sys.stdout = open('/dev/stdout', 'w')     # mode.
-sys.stderr = open('/dev/stderr', 'w') 
+# Re-open standard files in UTF-8 mode.
+sys.stdin = open('/dev/stdin', 'r')       
+sys.stdout = open('/dev/stdout', 'w')
+sys.stderr = open('/dev/stderr', 'w')
 
-
-# Atlassian namespaces
+# Atlassian namespaces, we don't really use them
 # http://www.amnet.net.au/~ghannington/confluence/docs/confluence/g-ri_confluence.ri.html
 NS={'ri':'http://www.atlassian.com/schema/confluence/4/ri/', 
     'ac':'http://www.atlassian.com/schema/confluence/4/ac/' }
@@ -94,8 +90,9 @@ NS={'ri':'http://www.atlassian.com/schema/confluence/4/ri/',
 # parse the XML config
 config = etree.parse("msConf.xml")
 
-# we somehow need to know here as well which category (external, beta, stable) this is in
-# in the tuple first is ID, second the title, third the category. We should get this from Confluence
+# we somehow need to know here as well which category (external, beta,
+# stable) this is in in the tuple first is ID, second the title, third
+# the category. We should get this from Confluence
 instUnits = {}
 for name, idno, longname, cat, confId in zip(config.xpath('//plugin/@name'),config.xpath('//plugin/id'),config.xpath('//plugin/longName'),config.xpath('//plugin/cat'),config.xpath('//plugin/confluenceID')):
     instUnits.update({name:(idno.text,longname.text,cat.text,confId.text)})
@@ -104,34 +101,19 @@ for name, idno, longname, cat, confId in zip(config.xpath('//plugin/@name'),conf
 # Confluence stuff #
 ####################
 def getConfluencePluginData(plugId):
-    """
-    Use python's lxml to get the Plugin pages from Confluence.
-    REST documentation on https://docs.atlassian.com/atlassian-confluence/REST/latest/
-
-    using curl, I can access restricted pages via command line:
-
-    curl -u KlausThoden -X GET https://dev2.dariah.eu/wiki/rest/prototype/1/content/27329537
-
-    """
+    """Get info about the plugins from the Confluence pages. The plugId
+    is found in the config file."""
     import urllib.request, urllib.parse, urllib.error
     import sys
 
-    # if state=="offline":
-    #     baseURL = "file:///Users/kthoden/TextGrid/Marketsplace/dev/"
-    #     plugId = "27329537"
-    # else:
     baseURL = "https://dev2.dariah.eu/wiki/rest/prototype/1/content/"
-    # plugId = "34341688" # digilib
-
     fullpath = baseURL + plugId
 
-    # sys.stdout.write("Getting info from %s.\n"% fullpath)
     usock=urllib.request.urlopen(fullpath)
 
     try:
         pluginInfo = etree.parse(usock)
     except etree.XMLSyntaxError:
-        # print("Online resource %s not found." % fullpath)
         sys.exit()
     usock.close()
 
@@ -139,7 +121,10 @@ def getConfluencePluginData(plugId):
 ## def getConfluencePluginData ends here
 
 def reverseTags(text):
-    """Escape html entities, just in case."""
+    """Deals with the escaped html markup in the page that is returned
+    from confluence. Re-establishes the XML tags and deletes the
+    namespaces.
+    """
     import re
 
     replacements = {
@@ -155,12 +140,12 @@ def reverseTags(text):
     for thing in list(replacements.keys()):
         text = text.replace(thing, replacements[thing])
     text = re.sub(r'&(?=\s)','&amp;',text)
-    
     return text
 # def reverseTags ends here
 
 def unescape(text):
-    """with thanks to http://effbot.org/zone/re-sub.htm#unescape-html"""
+    """With thanks to http://effbot.org/zone/re-sub.htm#unescape-html.
+    Modified to work with Python3."""
     import re,html.entities
 
     def fixup(m):
@@ -185,7 +170,8 @@ def unescape(text):
 # def unescape ends here
 
 def parseConfluenceBody(codedBody):
-    """Body needs to be transformed a bit."""
+    """Parse the page returned by getConfluencePluginData, fix the code
+    and read the relevant bits into XML again."""
     # some ugly modifications
     clean = reverseTags(codedBody)
     clean = unescape(clean)
@@ -196,24 +182,23 @@ def parseConfluenceBody(codedBody):
 ## def parseConfluenceBody ends here
 
 def compileInfo(pluginData):
-    """Return a dictionary of the things we parsed out of the webpage"""
-    pluginTitle = pluginData.xpath('/content/title')[0].text
+    """Return a dictionary of the things parsed out of the webpage"""
 
     # these are the guts we need
     pluginBody = pluginData.xpath('/content/body')[0].text
-    gingerAle = parseConfluenceBody(pluginBody)
+    pluginTable = parseConfluenceBody(pluginBody)
 
     pluginDict = dict({
-        "plTitle" : gingerAle.xpath('/table/tbody/tr[1]/td')[0].text,
-        "plDesc" : gingerAle.xpath('/table/tbody/tr[2]/td')[0].text,
-        "plIcon" : gingerAle.xpath('/table/tbody/tr[3]/td/image/attachment/@filename')[0],
-        "plMaturity" : gingerAle.xpath('/table/tbody/tr[4]/td/structured-macro/parameter[@name="colour"]')[0].text,
-        "plReq" : gingerAle.xpath('/table/tbody/tr[5]/td')[0].text,
+        "plTitle" : pluginTable.xpath('/table/tbody/tr[1]/td')[0].text,
+        "plDesc" : pluginTable.xpath('/table/tbody/tr[2]/td')[0].text,
+        "plIcon" : pluginTable.xpath('/table/tbody/tr[3]/td/image/attachment/@filename')[0],
+        "plMaturity" : pluginTable.xpath('/table/tbody/tr[4]/td/structured-macro/parameter[@name="colour"]')[0].text,
+        "plReq" : pluginTable.xpath('/table/tbody/tr[5]/td')[0].text,
         # OBS, license needs not be a link
-        "plLicense" : gingerAle.xpath('/table/tbody/tr[6]/td/a')[0].text,
-        "plSource" : gingerAle.xpath('/table/tbody/tr[7]/td/a')[0].text,
-        "plProjects" : gingerAle.xpath('/table/tbody/tr[8]/td')[0].text,
-        "plFiles" : gingerAle.xpath('/table/tbody/tr[9]/td')[0].text
+        "plLicense" : pluginTable.xpath('/table/tbody/tr[6]/td/a')[0].text,
+        "plSource" : pluginTable.xpath('/table/tbody/tr[7]/td/a')[0].text,
+        "plProjects" : pluginTable.xpath('/table/tbody/tr[8]/td')[0].text,
+        "plFiles" : pluginTable.xpath('/table/tbody/tr[9]/td')[0].text
     })
     return pluginDict
 # def compileInfo ends here
@@ -223,39 +208,42 @@ def compileInfo(pluginData):
 #############################################
 
 def buildMPapiP():
-    """Construct of one answer. Requires only info from config file."""
+    """Return info about the whole marketplace. Which categories are in there?"""
 
+    # parsing the config file
     msId = config.xpath('//general/id')[0].text
     msName = config.xpath('//general/name')[0].text
     msUrl = config.xpath('//general/url')[0].text
 
+    # building the XML
     mplace = etree.Element("marketplace")
     market = etree.SubElement(mplace,"market", id=msId, name=msName, url=msUrl+"category/markets/"+msId)
 
     catCount=1
-    # conCat = config["Categories"]
 
     cat = config.xpath('//category')
     catId = config.xpath('//category/@id')
 
+    # iterating through the categories
     for catKey,catVal in zip(cat,catId):
-        # Yay zip!
         etree.SubElement(market,"category",count=str(catCount), id=catVal,name=catKey.text, url=str(msUrl)+"taxonomy/term/"+msId+","+catKey.text)
         catCount += 1
     return mplace
 # def buildMPapiP ends here
 
 def buildMPcatApiP():
-    """The info on catalog. According to server log, this is the first
-    thing the Lab looks for. Requires only info from config file.
-    After choosing that catalog, the root is called.
+    """Return information on a catalog. According to server log, this is
+    the first thing the Lab looks for. Requires only info from config
+    file. After choosing that catalog, the root is called.
     """
+    # read from config
     msId = config.xpath('//general/id')[0].text
     msName = config.xpath('//general/name')[0].text
     msUrl = config.xpath('//general/url')[0].text
     msTitle = config.xpath('//general/title')[0].text
-    msIcon = config.xpath('//general/title')[0].text
+    msIcon = config.xpath('//general/icon')[0].text
 
+    # build the XML
     mplace = etree.Element("marketplace")
     mplace.append(etree.Comment("File generated on %s" % datetime.now().strftime("%Y-%m-%dT%H:%M:%S")))
     catalogs = etree.SubElement(mplace,"catalogs")
@@ -271,7 +259,9 @@ def buildMPcatApiP():
 # def buildMPcatApiP ends here
 
 def buildMPtaxonomy(marID,catID,instUnits):
-    """Construct the taxonomy. Lists all plugins of one category."""
+    """Construct the taxonomy. List all plugins of one category. The
+    category a plugin belongs to is taken from the config."""
+
     msUrl = config.xpath('//general/url')[0].text
 
     # small dictionary for handling the category name and id
@@ -279,11 +269,10 @@ def buildMPtaxonomy(marID,catID,instUnits):
     for catKey,catVal in zip(config.xpath('//category'),config.xpath('//category/@id')):
         catDict.update({catVal:catKey.text})
 
+    # build the XML
     mplace = etree.Element("marketplace")
-
-    # print("catalog dictionary", catDict["4"], type(catID))
     category = etree.SubElement(mplace,"category",id=str(catID),name=(catDict[catID]),url=msUrl+"taxonomy/term/"+str(marID)+","+str(catID))
-    # wiederhole die zwei nächsten für alle, die in der entsprechenden Gruppe sind
+    # repeat for thoese belonging to the same group
     for iu in instUnits.items():
         if int(iu[1][2]) == int(catID):
             node = etree.SubElement(category,"node",id=iu[1][0],name=iu[1][1],url=msUrl+"content/"+iu[1][0])
@@ -292,19 +281,18 @@ def buildMPtaxonomy(marID,catID,instUnits):
 # def buildMPtaxonomy ends here
 
 def buildMPnodeApiP(instUnits,plugName):
-    """info on installable Unit. Totally un-finished"""
+    """Return info on installable Unit (i.e. plugin). Totally un-finished"""
     msUrl = config.xpath('//general/url')[0].text
     msId = config.xpath('//general/id')[0].text
 
     # here we get the info about the plugin! returns a dictionary full of stuff
-    rawXML = getConfluencePluginData(instUnits[plugName][3])
+    plugId = instUnits[plugName][3]
+    rawXML = getConfluencePluginData(plugId)
     plug = compileInfo(rawXML)
 
-    node = etree.Element("node",id=instUnits[plugName][0], name=instUnits[plugName][1], url=msUrl+instUnits[plugName][0])
+    node = etree.Element("node",id=instUnits[plugName][0], name=instUnits[plugName][1], url=msUrl+"content/"+instUnits[plugName][0])
 
-    plDescUU = plug["plDesc"]
-    bodyEle = etree.SubElement(node,"body").text = etree.CDATA(plDescUU)
-    # bodyEle = etree.SubElement(node,"body").text = etree.CDATA(plug["plDesc"])
+    bodyEle = etree.SubElement(node,"body").text = etree.CDATA(plug["plDesc"])
     # taken from Label of wikipage
     catEle = etree.SubElement(node,"categories")
     # noch nicht ganz fertig!
@@ -322,7 +310,7 @@ def buildMPnodeApiP(instUnits,plugName):
     # 1 is original value here
     foundationEle = etree.SubElement(node,"foundationmember").text = "1"
     urlEle = etree.SubElement(node,"homepageurl").text = etree.CDATA("http://www.textgrid.de")
-    imageEle = etree.SubElement(node,"image").text = etree.CDATA("https://develop.sub.uni-goettingen.de/repos/textgrid/trunk/lab/noteeditor/info.textgrid.lab.noteeditor/icons/meise_64.png")
+    imageEle = etree.SubElement(node,"image").text = etree.CDATA("https://dev2.dariah.eu/wiki/download/attachments/" + plugId + "/" + plug["plIcon"])
     # just a container
     iusEle = etree.SubElement(node,"ius")
     # where to store that information?
@@ -333,21 +321,20 @@ def buildMPnodeApiP(instUnits,plugName):
     # what is this about?
     resourceEle = etree.SubElement(node,"resource") # this?
     # see logo
-    scrshotEle = etree.SubElement(node,"screenshot").text = "https://develop.sub.uni-goettingen.de/repos/textgrid/trunk/lab/noteeditor/info.textgrid.lab.noteeditor.feature/Screenshot_MEISE_2012-04-20.png"
+    #scrshotEle = etree.SubElement(node,"screenshot").text = "https://develop.sub.uni-goettingen.de/repos/textgrid/trunk/lab/noteeditor/info.textgrid.lab.noteeditor.feature/Screenshot_MEISE_2012-04-20.png"
     # also hidden field?
-    updateEle = etree.SubElement(node,"updateurl").text = etree.CDATA("http://download.digital-humanities.de/updates/textgridlab/noteeditor")
+    updateEle = etree.SubElement(node,"updateurl").text = etree.CDATA("http://download.digital-humanities.de/updates/textgridlab/" + plugName)
 
     return node
 # def buildMPnodeApiP ends here
 
-def buildMPfeaturedApiP(listType):
-    """this takes those nodes (my theory here) that have a value of
+def buildMPfrfpApiP(listType):
+    """Take those nodes (my theory here) that have a value of
     non-nil in 'featured' (should be on the wiki page) and wraps them
     into some XML
-    might also work for recent, favorite and popular, they are similar
+    Works also for recent, favorite and popular, they are similar.
     """
-    # the number of items, I think, not a fixed value
-
+    # this list needs to be somewhat dynamic
     featuredList = ["digilib"]
 
     mplace = etree.Element("marketplace")
@@ -358,17 +345,61 @@ def buildMPfeaturedApiP(listType):
         mplace.insert(1,xxx)
 
     return mplace
-# def buildMPfeaturedApiP ends here
+# def buildMPfrfpApiP ends here
+
+def buildMPcontentApiP(nodeId,instUnits):
+    """Return info on a single node."""
+
+    auxdic = dict()
+    for iu in instUnits.items():
+        auxdic.update({iu[1][0] : iu[0]})
+
+    mplace = etree.Element("marketplace")
+    xxx = buildMPnodeApiP(instUnits,auxdic[nodeId])
+    mplace.insert(1,xxx)
+
+    return mplace
+
+# def buildMPcontentApiP ends here
+
+##########
+# Output #
+##########
+def goToConfluence(nodeId,instUnits):
+    """Redirect the browser to the Confluence page."""
+
+    auxdic = dict()
+    for iu in instUnits.items():
+        auxdic.update({iu[1][0] : iu[1][3]})
+
+    gotoURL = "https://dev2.dariah.eu/wiki/pages/viewpage.action?pageId=" + auxdic[nodeId]    
+
+    print('Status: 303 See Other')
+    # newline is important here
+    print('Location: ' + gotoURL + '\n')
+# def goToConfluence ends here
+
+def outputXML(node):
+    """Serve the XML for the Marketplace. Here you go."""
+    # output
+    print('Content type: text/xml; charset=utf-8\n')
+    # this is of a bytes type
+    son = etree.tostring(node,pretty_print=True,encoding='utf-8',xml_declaration=True)
+    # convert this to a string
+    outDecode = son.decode('utf-8')
+    # for debugging
+    print(outDecode)
+# def outputXML ends here
 
 ################
 # The main bit #
 ################
-
 def main():
     parser = argparse.ArgumentParser()
     msActions = parser.add_mutually_exclusive_group()
     msActions.add_argument("-a","--action",help="call the function",action="store_true")
 
+    # other arguments that we might somewhat use but so far don't
     parser.add_argument("-ty","--type",help="type of list to be generated")
     parser.add_argument("-cid","--categoryId",help="ID of category")
     parser.add_argument("-mid","--marketId",help="ID of market")
@@ -386,33 +417,36 @@ def main():
     parser.add_argument("--nl",help="Language")
 
     args=parser.parse_args()
-
+    # arguments need to be read into something that the CGI can deal with
     form = cgi.FieldStorage()
     if form.getvalue('action') == 'main':
         node = buildMPapiP()
+        outputXML(node)
     if form.getvalue('action') == 'catalogs':
         node = buildMPcatApiP()
+        outputXML(node)
     if form.getvalue('action') == 'taxonomy':
         node = buildMPtaxonomy(form.getvalue('marketId'),form.getvalue('categoryId'),instUnits)
+        outputXML(node)
     # list covers all of recent, favorites, popular and featured
     if form.getvalue('action') == 'list':
-        node = buildMPfeaturedApiP(form.getvalue('type'))
-
+        node = buildMPfrfpApiP(form.getvalue('type'))
+        outputXML(node)
     if form.getvalue('action') == 'content':
-        node = "not yet implemented"
+        node = buildMPcontentApiP(form.getvalue('nodeId'),instUnits)
+        outputXML(node)
+    if form.getvalue('action') == 'redirect':
+        goToConfluence(form.getvalue('nodeId'),instUnits)
+
+    # really bad error handling
     if form.getvalue('action') == 'node':
         node = "not yet implemented"
     if form.getvalue('action') == 'search':
         node = "not yet implemented"
 
-    # output
-    print('Content type: text/xml; charset=utf-8\n')
-    # this is of a bytes type
-    son = etree.tostring(node,pretty_print=True,encoding='utf-8',xml_declaration=True)
-    # convert this to a string
-    outDecode = son.decode('utf-8')
-    # for debugging
-    print(outDecode)
-
 if __name__ == "__main__":
     main()
+
+#########
+# FINIS #
+#########
