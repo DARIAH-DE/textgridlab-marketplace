6c2d63a8bf65de67c993267e8da9757ebe23db6a
☞ <kthoden@mpiwg-berlin.mpg.de>
2015-04-09 11:17:03 +0200

    Added license.
    Going now with GPL.

--- msInterface.py
+++ msInterface.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8; mode: python -*-
 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
 # Tips von Thorsten
 # ans Monitoring anschließen: wenn die Anfrage OK zurückkommt, dann läufts ok
 # Konfigurationsdatei auch ins Confluence tun
@@ -43,9 +58,6 @@ Some new stuff:
 - we get the list from there
 
 """
-
-# what is the license??
-
 __author__ = "Klaus Thoden, kthoden@mpiwg-berlin.mpg.de"
 __date__ = "2015-02-13"
 
